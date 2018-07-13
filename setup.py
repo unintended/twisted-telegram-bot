9486e943b85615e4b0a1c267c96e316117fa87c0
unintended <unintended@users.noreply.github.com>
2018-07-13 11:14:00 +0300

    1.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ packages = find_packages()
 
 setup(
   name='twisted-telegram-bot',
-  version="1.3.0",
+  version="1.4.0",
   description="Asynchronous Twisted-based Telegram Bot API for Python",
   author='unintended',
   author_email='unintended.github@gmail.com',
