ba51f0c645046e6034b3bf188001590b2d237b74
unintended <unintended@users.noreply.github.com>
2019-01-11 15:19:36 +0300

    2.1.3

--- ttbot/__init__.py
+++ ttbot/__init__.py
@@ -85,6 +85,7 @@ def _map_function_to_deferred(f, *args, **kwargs):
 
 class TelegramBot(object):
   def __init__(self, token, name, skip_offset=False, allowed_updates=None, agent=None, timeout=None):
+    self.id = int(token.split(':')[0])
     self.name = name
     self.token = token
     self.agent = agent
